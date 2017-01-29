af78bd2c0332e7651ac328feca4812c7d2d82cfd
kgashok <lifebalance@gmail.com>
2017-01-29 17:02:32 +0000

    forgot to add Qna.elm file

--- Qna.elm
+++ Qna.elm
@@ -173,12 +173,26 @@ viewAllAnswers: Model -> Html Msg
 viewAllAnswers model =
   let 
     listOfAnswers = model.answer 
-      |> List.sortBy .confidence 
-      |> List.reverse
+      |> List.sortWith (descending .confidence)
+      --|> List.sortBy .confidence 
+      --|> List.reverse
       |> List.map viewAnswer 
   in 
     ul [] listOfAnswers  
 
+
+descending : (a -> comparable) -> (a -> a -> Order)
+descending toComparable x y =
+  let
+    flippedComparison a b =
+      case compare a b of
+        LT -> GT
+        EQ -> EQ
+        GT -> LT
+  in
+    flippedComparison (toComparable x) (toComparable y)
+    
+    
 viewAnswer: Answer -> Html Msg 
 viewAnswer answer = 
   li []
@@ -265,3 +279,4 @@ getReponse =
     |> Http.request settings
     |> Http.send NewResponse 
 --}
+
